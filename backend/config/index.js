84cc5a3e974898a72edca590c7a4b04e5ad40502
kunalkeshan <kunalkeshan12@gmail.com>
2022-03-18 08:25:53 +0530

    Added production variables

--- backend/config/index.js
+++ backend/config/index.js
@@ -8,12 +8,14 @@ dotenv.config();
 
 const isProduction = process.env.NODE_ENV === 'production';
 const dbUrl = isProduction ? process.env.MONGODB_URL : 'mongodb://localhost:27017/TeamKoalaSIH'
+const reactAppUrl = isProduction ? process.env.REACT_APP_URL : 'http://localhost:3000';
 
 // Configuration Container
 const configuration = {
     PORT: parseInt(process.env.PORT, 10),
     isProduction,
     dbUrl,
+    reactAppUrl,
     secrets: {
         jwtSecret: process.env.JWT_SECRET,
         cookieSecret: process.env.COOKIE_SECRET,
--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const client = axios.create({
-    baseURL: 'http://localhost:5000'
-});
+const baseURL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BACKEND_URL : 'http://localhost:5000';
+
+const client = axios.create({ baseURL });
 
 const backend = async (options) => {
     const onSuccess = response => response.data;
